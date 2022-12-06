586a021eaed1e94bef55df8ee98dc1a2ab649b6a
jalp21@icloud.com <jalp21@icloud.com>
2022-12-05 19:57:10 -0800

    edit and update routes completed

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -12,6 +12,17 @@ breads.get('/', (req, res) => {
   // res.send(Bread)
 })
 
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+    if(req.body.hasGluten === 'on'){
+      req.body.hasGluten = true
+    } else {
+      req.body.hasGluten = false
+    }
+    Bread[req.params.arrayIndex] = req.body
+    res.redirect(`/breads/${req.params.arrayIndex}`)
+  })
+  
 // CREATE
 breads.post('/', (req, res) => {
     if (!req.body.image) {
@@ -25,7 +36,6 @@ breads.post('/', (req, res) => {
     Bread.push(req.body)
     res.redirect('/breads')
   })
-  
 
 // NEW
 breads.get('/new', (req, res) => {
@@ -37,7 +47,15 @@ breads.delete('/:indexArray', (req, res) => {
     Bread.splice(req.params.indexArray, 1)
     res.status(303).redirect('/breads')
   })
-  
+
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+      bread: Bread[req.params.indexArray],
+      index: req.params.indexArray
+    })
+})
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
@@ -49,8 +67,6 @@ breads.get('/:arrayIndex', (req, res) => {
       res.render('404')
     }
   })
-  
-  
   
 module.exports = breads
 
