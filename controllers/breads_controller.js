ed784ab537b5ee4adbe7b7d07130e94c73e0bb30
jalp21@icloud.com <jalp21@icloud.com>
2022-12-05 19:51:09 -0800

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -32,16 +32,24 @@ breads.get('/new', (req, res) => {
     res.render('new')
 })
 
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+    Bread.splice(req.params.indexArray, 1)
+    res.status(303).redirect('/breads')
+  })
+  
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
       res.render('Show', {
-        bread:Bread[req.params.arrayIndex]
+        bread:Bread[req.params.arrayIndex],
+        index: req.params.arrayIndex,
       })
     } else {
-      res.send('404')
+      res.render('404')
     }
   })
+  
   
   
 module.exports = breads
